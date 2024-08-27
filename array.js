4ec65bf88fee689ab0f451d681630d9d91b8368e
julieg18 <juliannagal18@gmail.com>
2024-08-27 16:17:43 -0500

    Add array and linkedList files

---
+++ array.js
@@ -0,0 +1,8 @@
+const list = [1, 2, 3]
+const result = list[6]
+
+list.push(4)
+
+if (list.includes(7)) {
+  console.log('it has it')
+}
---
+++ linkedList.js
@@ -0,0 +1,140 @@
+class Node {
+  constructor(data = null, nextNode = null) {
+    this.data = data
+    this.nextNode = nextNode
+  }
+
+  view() {
+    return `<Node data: ${this.data}>`
+  }
+
+  setNextNode(node) {
+    this.nextNode = node
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+
+  isEmpty() {
+    // Takes O(1)
+    return this.head === null
+  }
+
+  getLength() {
+    // Takes O(n) time
+    let current = this.head
+    let count = 0
+    while (current) {
+      count += 1
+      current = current.nextNode
+    }
+
+    return count
+  }
+
+  unshift(data) {
+    const newNode = new Node(data)
+    newNode.nextNode = this.head
+    this.head = newNode
+  }
+
+  view() {
+    // Takes O(n) time
+    const nodes = []
+    let current = this.head
+
+    while (current) {
+      if (current === this.head) {
+        nodes.push(`[Head: ${current.data}]`)
+      } else if (current.nextNode === null) {
+        nodes.push(`[Tail: ${current.data}]`)
+      } else {
+        nodes.push(`[${current.data}]`)
+      }
+
+      current = current.nextNode
+    }
+
+    return nodes.join('-> ')
+  }
+
+  find(key) {
+    // Takes O(n) time
+    let current = this.head
+
+    while (current) {
+      if (current.data === key) {
+        return current
+      } else {
+        current = current.nextNode
+      }
+    }
+
+    return undefined
+  }
+
+  insert(data, index) {
+    // inserting takes O(1) but finding the node
+    // takes O(n). Thus, overall time is linear.
+    if (index === 0) {
+      return this.add(data)
+    }
+
+    const newNode = new Node(data)
+
+    let position = index
+    let current = this.head
+
+    while (position > 1) {
+      current = current.nextNode
+      position--
+    }
+
+    const prevNode = current
+    const nextNode = current.nextNode
+    prevNode.nextNode = newNode
+    newNode.nextNode = nextNode
+  }
+
+  remove(key) {
+    // O(n)
+    let current = this.head
+    let previous = null
+    let found = false
+
+    while (current && !found) {
+      if (current.data === key && current === this.head) {
+        found = true
+        this.head = current.nextNode
+      } else if (current.data === key) {
+        found = true
+        previous.nextNode = current.nextNode
+      } else {
+        previous = current
+        current = current.nextNode
+      }
+    }
+
+    return current
+  }
+}
+
+const list = new LinkedList()
+list.unshift(1)
+list.unshift(1)
+list.unshift(1)
+list.unshift(1)
+list.unshift(5)
+
+console.log(list.view())
+
+list.insert(7, 3)
+
+console.log(list.view())
+
+list.remove(7)
+
+console.log(list.view())
